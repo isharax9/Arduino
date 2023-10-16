f174fe281a16b891e247c9683f811477e3e85ee0
mac_knight141 <123813453+isharax9@users.noreply.github.com>
2023-10-16 23:47:37 +0530

    Servo Motor 180 degree Rotation loop

---
+++ understanding_servo_motor_functionality_1.ino
@@ -0,0 +1,32 @@
+// C++ code
+
+#include <Servo.h>
+
+int pos = 0;
+
+Servo servo_9;
+
+void setup()
+{
+  servo_9.attach(9, 500, 2500);
+  pinMode(LED_BUILTIN, OUTPUT);
+}
+
+void loop()
+{
+  // sweep the servo from 0 to 180 degrees in steps
+  // of 1 degrees
+  for (pos = 0; pos <= 180; pos += 1) {
+    // tell servo to go to position in variable 'pos'
+    servo_9.write(pos);
+    // wait 15 ms for servo to reach the position
+    digitalWrite(LED_BUILTIN, HIGH);
+    delay(15); // Wait for 15 millisecond(s)
+  }
+  for (pos = 180; pos >= 0; pos -= 1) {
+    // tell servo to go to position in variable 'pos'
+    servo_9.write(pos);
+    // wait 15 ms for servo to reach the position
+    delay(15); // Wait for 15 millisecond(s)
+  }
+}
