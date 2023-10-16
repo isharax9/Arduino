adc0768e5d1ac22008325ca64cbb9b87c2afb853
mac_knight141 <123813453+isharax9@users.noreply.github.com>
2023-10-16 23:31:03 +0530

    Night-Rider 5 LED circuit

---
+++ NightRider 5 LED.ino
@@ -0,0 +1,66 @@
+// C++ code
+//
+int x = 100;
+int y = 75;
+void loop()
+void setup()
+
+{
+  pinMode(13, OUTPUT);
+  pinMode(12, OUTPUT);
+  pinMode(11, OUTPUT);
+  pinMode(10, OUTPUT);
+  pinMode(9, OUTPUT);
+  
+  
+}
+
+{
+  digitalWrite(8,HIGH);
+  delay(x);
+  digitalWrite(8,LOW);
+  delay(y);
+  digitalWrite(9,HIGH);
+  delay(x);
+  digitalWrite(9,LOW);
+  delay(y);
+  digitalWrite(10,HIGH);
+  delay(x);
+  digitalWrite(10,LOW);
+  delay(y);
+  digitalWrite(11,HIGH);
+  delay(x);
+  digitalWrite(11,LOW);
+  delay(y);
+  digitalWrite(12,HIGH);
+  delay(x);
+  digitalWrite(12,LOW);
+  delay(y);
+  digitalWrite(13,HIGH);
+  delay(x);
+  digitalWrite(13,LOW);
+  delay(y);
+  
+  digitalWrite(13,HIGH);
+  delay(x);
+  digitalWrite(13,LOW);
+  delay(y);
+  digitalWrite(12,HIGH);
+  delay(x);
+  digitalWrite(12,LOW);
+  delay(y);
+  digitalWrite(11,HIGH);
+  delay(x);
+  digitalWrite(11,LOW);
+  delay(y);
+  digitalWrite(10,HIGH);
+  delay(x);
+  digitalWrite(10,LOW);
+  delay(y);
+  digitalWrite(9,HIGH);
+  delay(x);
+  digitalWrite(9,LOW);
+  delay(y);
+  
+  
+}
